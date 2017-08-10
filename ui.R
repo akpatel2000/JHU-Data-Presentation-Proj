ec93cc8cdb5bf83d1874c994aa68f84fb1356e09
AK Patel <akpatel2000@gmail.com>
2017-08-10 15:18:01 -0400

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,23 @@
+library(shiny)
+library(lubridate)
+
+shinyUI(fluidPage(
+  titlePanel("Compare Treasury Yield Curve from Two Different Dates"),
+  sidebarLayout(
+    sidebarPanel(
+      h4("Yield Curve 1", style = "color:#8DFF33"),
+      dateInput("date1", "Enter Date:", value = as.Date("2016-12-21")),
+      h4("Yield Curve 2", style = "color:blue"),
+      sliderInput("slider1", "Use Slider to look back # of days", 1, 1000, 200)
+
+
+
+    ),
+    mainPanel(
+        tabsetPanel(type="tabs",
+                tabPanel("Plot",plotOutput("plot1")),
+                tabPanel("Documentation", verbatimTextOutput("Documentation"))
+        )
+    )
+  )
+))
